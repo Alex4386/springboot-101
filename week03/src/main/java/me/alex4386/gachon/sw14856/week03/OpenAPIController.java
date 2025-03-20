a5effbd50f350b07ef1d7efa97b308ecb4f6730f
Alex4386 <psh010209@gmail.com>
2025-03-20 14:54:23 +0900

    feat: add scalar api docs using thymeleaf

--- week03/src/main/java/me/alex4386/gachon/sw14856/week03/OpenAPIController.java
+++ week03/src/main/java/me/alex4386/gachon/sw14856/week03/OpenAPIController.java
@@ -2,35 +2,23 @@ package me.alex4386.gachon.sw14856.week03;
 
 
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class OpenAPIController {
-    public static String generateScalarDocs(String endpoint) {
-        return "<!doctype html>\n" +
-                "<html>\n" +
-                "  <head>\n" +
-                "    <title>Scalar API Reference</title>\n" +
-                "    <meta charset=\"utf-8\" />\n" +
-                "    <meta\n" +
-                "      name=\"viewport\"\n" +
-                "      content=\"width=device-width, initial-scale=1\" />\n" +
-                "  </head>\n" +
-                "  <body>\n" +
-                "    <!-- Need a Custom Header? Check out this example: https://codepen.io/scalarorg/pen/VwOXqam -->\n" +
-                "    <script\n" +
-                "      id=\"api-reference\"\n" +
-                "      data-url=\""+endpoint+"\"></script>\n" +
-                "\n" +
-                "    <script src=\"https://cdn.jsdelivr.net/npm/@scalar/api-reference\"></script>\n" +
-                "  </body>\n" +
-                "</html>";
-    }
+
+    @Value("${springdoc.api-docs.path}")
+    private String openApiUrl;
 
     @GetMapping("/")
     @Hidden
-    public String apiSpec() {
-        return generateScalarDocs("/swagger-ui");
+    public String apiSpec(Model model) {
+        model.addAttribute("openApiUrl", openApiUrl);
+
+        return "scalar";
     }
 }
